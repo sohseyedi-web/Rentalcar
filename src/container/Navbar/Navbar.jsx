879f5ffb4671ebd354d82edcffbc85d7af8fcbbf
sohseyedi <sohseyedi@gmail.com>
2022-11-21 11:36:15 +0330

    responsive complete

--- src/container/Navbar/Navbar.jsx
+++ src/container/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/jeep.png";
+import { RiFingerprint2Line } from "react-icons/ri";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
   return (
     <nav className="header">
       <Link to="/" className="header-logo">
         <img src={Logo} alt="jeep" />
       </Link>
-      <ul className="header-nav active-menu">
+      <ul className={active ? "header-nav active-nav" : "header-nav"}>
         <li className="header-nav__item">
           <Link to="/">خانه</Link>
         </li>
@@ -22,8 +26,10 @@ const Navbar = () => {
           <Link to="/">درباره ما</Link>
         </li>
       </ul>
-      <div className="header-menu" id="menu">
-        <span className="bx bx-menu"></span>
+      <div className="header-menu" onClick={() => setActive(!active)}>
+        <span>
+          <RiFingerprint2Line />
+        </span>
       </div>
       <div className="header-auth">
         <Link to="/register" className="header-auth__register">
